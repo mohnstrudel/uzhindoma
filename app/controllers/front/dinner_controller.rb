504272301427beae4781445b024bb98067602706
Anton Kostin <schnibba@gmail.com>
2016-06-13 18:07:25 +0300

    Add check and corresponding error output if current menu is empty.

--- app/controllers/front/dinner_controller.rb
+++ app/controllers/front/dinner_controller.rb
@@ -2,10 +2,13 @@ class Front::DinnerController < FrontController
 	def index
 		@categories = Category.all
 		@current_menus = Menu.current
-		@menu = @current_menus.first
-		@order = Order.new(menu_id: @menu.id)
-		@dessert_price = nil
-		@dessert_price ||= @current_menus[2].price
+
+		unless @current_menus.empty?
+			@menu = @current_menus.first
+			@order = Order.new(menu_id: @menu.id)
+			@dessert_price = nil
+			@dessert_price ||= @current_menus[2].price
+		end
 		
 	end
 
