7d364443c05b93b53ea2e1479764c125ecd773d5
Anton Kostin <schnibba@gmail.com>
2016-09-07 20:49:15 +0300

    Add Instagram API call on tags (working only with own pictures though).

--- app/controllers/front/static_pages_controller.rb
+++ app/controllers/front/static_pages_controller.rb
@@ -1,9 +1,17 @@
 class Front::StaticPagesController < FrontController
   def home
-  	client = Instagram.client(access_token: "3402426957.237f14b.bb3b43cb6d74479782d37d9742b786ea")
+    token = "3402426957.237f14b.bb3b43cb6d74479782d37d9742b786ea"
+  	client = Instagram.client(access_token: token)
   	# user = client.user
-  	@instagram = client.user_recent_media
+    # tags = client.tag_search('unfiltered')
+    tags = URI.escape("ужиндома_тестдрайв")
+    url = "https://api.instagram.com/v1/tags/#{tags}/media/recent?access_token=#{token}"
+  	doc = Nokogiri::HTML(open(url))
+    
+    @instagram = JSON.parse(doc)["data"]
 
+    # @instagram = client.tag_recent_media(tags[0].name)
+    # debug
   	@current_menus = Menu.current
 
   	@setting = Setting.first
