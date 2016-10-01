653ee8ab928f0826a66408da1a4d9b2b68b909ca
Anton Kostin <schnibba@gmail.com>
2016-10-01 23:47:30 +0300

    Further config changes.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  if Rails.env.production?
-    config_path = File.expand_path(Rails.root.to_s + '/config/application.yml')
-      if File.exists? config_path
-      ENV.update YAML.load_file(config_path)[Rails.env]
-    end
-  end
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -95,7 +88,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'http://uzhindoma.eve-trader.net' }
   
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true 
+  # config.action_mailer.perform_deliveries = true 
   
   config.action_mailer.delivery_method = :smtp
 
