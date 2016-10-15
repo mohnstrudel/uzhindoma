419aaa497213ec4bfd417550eb312ffb6cb26fdd
Anton Kostin <schnibba@gmail.com>
2016-10-15 19:58:18 +0300

    Change the second page - mostly layout changes.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -12,7 +12,6 @@
 //= require jquery
 //= require jquery_ujs
 
-//= require_tree ../../../vendor/assets/javascripts/admin/jquery_ui/.
 //= require_tree ../../../vendor/assets/javascripts/admin/plugins/.
 //= require_tree ../../../vendor/assets/javascripts/admin/daterange/.
 //= require_tree ../../../vendor/assets/javascripts/admin/utility/.
