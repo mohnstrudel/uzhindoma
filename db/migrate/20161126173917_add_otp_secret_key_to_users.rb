bb1bcbd04bb95f5473f85b5905841bcfc13ac516
Anton Kostin <schnibba@gmail.com>
2016-11-27 00:49:45 +0300

    Fixed migration file.

--- db/migrate/20161126173917_add_otp_secret_key_to_users.rb
+++ db/migrate/20161126173917_add_otp_secret_key_to_users.rb
@@ -2,7 +2,4 @@ class AddOtpSecretKeyToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :otp_secret_key, :string
   end
-
-  User.all.each { |user| user.update_attribute(:otp_secret_key, ROTP::Base32.random_base32) }
-  
 end
