04921435aeb6276e5f9dda8935c6b6d320def66e
Anton Kostin <schnibba@gmail.com>
2016-11-30 20:23:00 +0300

    Add check for present dessert (and if not, set the values to 0

--- app/controllers/front/dinner_controller.rb
+++ app/controllers/front/dinner_controller.rb
@@ -2,13 +2,19 @@ class Front::DinnerController < FrontController
 	def index
 		@categories = Category.all
 		@current_menus = Menu.current.sort_by { |menu| menu.category.sortable }
-		@dessert = Menu.dessert[0]
+		@dessert = Menu.current_dessert[0]
+
+		if @dessert.nil?
+			@col_value = 6
+		else
+			@col_value = 4
+		end
 
 		unless @current_menus.empty?
 			@menu = @current_menus.first
 			@order = Order.new(menu_id: @menu.id)
-			@dessert_price = nil
-			unless Menu.dessert[0].nil?
+			@dessert_price = 0
+			unless @dessert.nil?
 				@dessert_price ||= Menu.dessert[0].price
 			end
 		end
