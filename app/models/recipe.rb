46652a1bda717433bf9c0fd71233c683507e7066
Anton Kostin <schnibba@gmail.com>
2016-11-30 20:30:11 +0300

    Add dependant destroy to recipes.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ActiveRecord::Base
 	has_many :pictures, dependent: :destroy
 	accepts_nested_attributes_for :pictures, allow_destroy: true
 
-	has_many :menurecipes
+	has_many :menurecipes, dependent: :destroy
 	has_many :menus, through: :menurecipes
 
 	accepts_nested_attributes_for :menurecipes
