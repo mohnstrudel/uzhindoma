cfa4a892ecf2c4f2b92548f33f84d95812a26a31
Anton Kostin <schnibba@gmail.com>
2017-02-07 23:52:31 +0300

    remove figaro part from cap recipe.

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,17 +50,6 @@ namespace :puma do
   before :start, :make_dirs
 end
 
-namespace :figaro do
-  desc "SCP transfer figaro configuration to the shared folder"
-  task :setup do
-    transfer :up, "config/application.yml", "#{shared_path}/application.yml", :via => :scp
-  end
-
-  desc "Symlink application.yml to the release path"
-  task :finalize do
-    run "ln -sf #{shared_path}/application.yml #{release_path}/config/application.yml"
-  end
-end
 
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
