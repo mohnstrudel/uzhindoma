ca9a8ebf42ed4ff3bea9689419f1e22372898cc6
Anton Kostin <schnibba@gmail.com>
2017-04-20 20:04:11 +0300

    Add SSL certificate.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # config.force_ssl = true
+
   # Do not eager load code on boot.
   config.eager_load = false
 
