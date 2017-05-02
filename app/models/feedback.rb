2e5613c52d89ab706c8d6724a9117b480a9807ff
Anton Kostin <schnibba@gmail.com>
2017-05-02 08:37:47 +0300

    Fix dashboard error.

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,7 +2,9 @@ class Feedback < ApplicationRecord
   
   def self.mean
     ary = all.map{|item| item.rating}
+    ary = ary.compact
     if ary.length > 0
+      debug
       mean = ary.sum / ary.length
       return mean
     else
