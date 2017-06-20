0831d5efb4a65a55298ec7d268801a863ed0c3de
Anton Kostin <schnibba@gmail.com>
2017-06-20 22:20:19 +0300

    Blog posts index refactoring. Add sanitizer to blog body.

--- app/helpers/front/posts_helper.rb
+++ app/helpers/front/posts_helper.rb
@@ -1,2 +1,8 @@
 module Front::PostsHelper
+  def article_description(text, length=200)
+    # desc = sanitize(article.description.html_safe)
+    # output = truncate(desc, length: 200, escape: false)
+    output = h truncate(text, length: length, omission: ' ... ', escape: false)
+    output.html_safe
+  end
 end
