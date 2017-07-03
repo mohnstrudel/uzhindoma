0613aa1ad1d2640ccdf22e55d5457ffc3c039ac9
Anton Kostin <schnibba@gmail.com>
2017-07-03 18:12:22 +0300

    Remove roistat.

--- app/assets/javascripts/front/social_scripts.js
+++ app/assets/javascripts/front/social_scripts.js
@@ -64,13 +64,13 @@ fbq('track', 'PageView');
 
 
 // roistat
-
-(function(w, d, s, h, id) {
-    w.roistatProjectId = id; w.roistatHost = h;
-    var p = d.location.protocol == "https:" ? "https://" : "http://";
-    var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init";
-    var js = d.createElement(s); js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
-})(window, document, 'script', 'cloud.roistat.com', 'a6da58db9cb636a28dd43ef27eb050fd');
+// Отключаем ройстат 03/07/2017 по запросу Н.Брагина
+// (function(w, d, s, h, id) {
+//     w.roistatProjectId = id; w.roistatHost = h;
+//     var p = d.location.protocol == "https:" ? "https://" : "http://";
+//     var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init";
+//     var js = d.createElement(s); js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
+// })(window, document, 'script', 'cloud.roistat.com', 'a6da58db9cb636a28dd43ef27eb050fd');
 
 
 // Mail.ru Rating
