ef7e3948608d174991740a79863ece7969448570
Anton Kostin <schnibba@gmail.com>
2018-04-18 14:45:13 +0300

    Дата доставки дачного меню обновлена.

--- app/helpers/front/dinner_helper.rb
+++ app/helpers/front/dinner_helper.rb
@@ -2,7 +2,8 @@ module Front::DinnerHelper
 
   def get_delivery_date(object)
     if object.category.name == "Дачное меню"
-      return "возможна 09.06.2017"
+      # return "возможна 09.06.2017"
+      return object.daterange.split("-")[1].gsub("/",".").gsub(" ", "")
     else
       return object.daterange.split("-")[1].gsub("/",".").gsub(" ", "")
     end
