4a30bc3c9d27e21da8b2ba4c3fd3f9afe5b49956
Anton Kostin <schnibba@gmail.com>
2018-05-26 15:31:24 +0300

    Migration version.

--- db/migrate/20170531160310_create_bootsy_image_galleries.bootsy.rb
+++ db/migrate/20170531160310_create_bootsy_image_galleries.bootsy.rb
@@ -1,6 +1,6 @@
 # This migration comes from bootsy (originally 20120628124845)
 # frozen_string_literal: true
-class CreateBootsyImageGalleries < ActiveRecord::Migration
+class CreateBootsyImageGalleries < ActiveRecord::Migration[5.0]
   def change
     create_table :bootsy_image_galleries do |t|
       t.references :bootsy_resource, polymorphic: true
