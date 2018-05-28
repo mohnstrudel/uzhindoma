0b9dde3a62093900be57be35f8818bcc30f7084a
Anton Kostin <schnibba@gmail.com>
2018-05-28 10:57:48 +0300

    Настроен редирект на уровне приложения.

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,7 +1,19 @@
 class FrontController < ApplicationController
 	layout 'front'
 
+  before_action :redirect_if_old
+
 	def fast_order
 		@order = Order.new
 	end
+
+  protected 
+
+  def redirect_if_old
+    if request.host == 'uzhin-doma.ru'
+      redirect_to "#{request.protocol}uzhindoma.ru#{request.fullpath}", :status => :moved_permanently 
+    elsif request.host == 'www.uzhin-doma.ru'
+      redirect_to "#{request.protocol}uzhindoma.ru#{request.fullpath}", :status => :moved_permanently 
+    end
+  end
 end
